e29eb3fe96a0ae8e748cb336b3a23fa88210a3f3
Vitryssen <andre1.nordlund@gmail.com>
2021-01-27 17:08:28 +0100

    Adding support for commands -qpl and -pf

--- src/labb1/Chat.java
+++ src/labb1/Chat.java
@@ -17,29 +17,44 @@ public class Chat {
     /**
      * @param args the command line arguments
      */
+    private static String currentUser = "Eurakarte";
     public static void main(String[] args) {
         // TODO code application logic here
         try{
+            if(args.length == 0){
+                System.out.println("-pf: Prints a list of the friends nicknames and real names ordered by nicknames.");
+                System.out.println("-qf nickname: Prints all the information related to the given nickname.");
+                System.out.println("-ql nickname: Displays the private chat log related to the given nickname.");
+                System.out.println("-qpl: Displays the public chat log.");  
+                return;
+            }
             switch(args[0]) 
             { 
-                case "-pf" -> System.out.println("one");  //Prints a list of the friends nicknames and real names ordered by nicknames
-                case "-qf" -> { 
-                    System.out.println("two");  //-qf nickname | prints all the information related to the given nickname or Not found
-                    System.out.println(args[1]);
-                }
-                case "-ql" -> System.out.println("three");
-                //or Not found
-                //-ql nickname | display private chat log to given nickname
-                case "-qpl" -> { 
+                case "-pf" -> {
+                    FriendsReader friends = new FriendsReader();
+                    friends.GetFriends();
+                }  //Prints a list of the friends nicknames and real names ordered by nicknames
+                case "-qf" -> { //-qf nickname | display all info about current user or Not found
                     if(!args[1].isEmpty()){
-                        PublicReader publicParser = new PublicReader(args[1]);
+                        LogReader publicParser = new LogReader(args[1]);
                         publicParser.GetHistory();
                     }
                     else
-                        System.out.println("Not found");
+                        System.out.println("Missing argument for nickname");
                 }
-                //-qpl <nickname of current user>.log prints the information of the public chat log or "Not found"
-                default -> System.out.println("no match"); //print help message describing the parameters
+                case "-ql" -> { //-ql nickname | display private chat log or Not found
+                    if(!args[1].isEmpty()){
+                        LogReader publicParser = new LogReader(args[1]);
+                        publicParser.GetHistory();
+                    }
+                    else
+                        System.out.println("Missing argument for nickname");
+                }
+                case "-qpl" -> { 
+                    LogReader publicParser = new LogReader(currentUser);
+                    publicParser.GetHistory();
+                }
+                default -> System.out.println("Invalid argument(s)");
             } 
         }
         catch(Exception e){
